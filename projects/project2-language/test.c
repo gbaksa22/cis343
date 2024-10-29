f85cc1830512e36cf536b6adb807a9a6b9c86ac7
gbaksa22 <gbaksa22@gmail.com>
2024-10-29 12:13:00 -0400

    started on sample script for testing

---
+++ projects/project2-language/test.c
@@ -0,0 +1,5 @@
+new $income 100 #myjob (10/29/2024) side hustle done
+new $income 200 #otherjob (10-29-2024) main job done
+
+new $expense 50 #groceries (10/29/2024) food done
+new $expense 100 #school (10-29-2024) textbook done
