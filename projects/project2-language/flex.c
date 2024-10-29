e73de7be41d6f70654e61b915939806f7dd58614
gbaksa22 <gbaksa22@gmail.com>
2024-10-29 11:39:15 -0400

    added escape to $

--- projects/project2-language/flex.c
+++ projects/project2-language/flex.c
@@ -12,8 +12,8 @@ new                                 { return NEW; }
 [0-9]+\.[0-9]{2}.                   { yylval.number = atof(yytext); return AMOUNT; }
 \$income.                            { yylval.string = yytext; return INCOME; }
 \$\+.                                { yylval.string = yytext; return INCOME; }
-$expense.                           { yylval.string = yytext; return EXPENSE; }
-$\-.                                { yylval.string = yytext; return EXPENSE; }
+\$expense.                           { yylval.string = yytext; return EXPENSE; }
+\$\-.                                { yylval.string = yytext; return EXPENSE; }
 \([0-9]{2}-[0-9]{2}-[0-9]{4}\).     { yylval.string = yytext; return DATE; }
 \([0-9]{2}\/[0-9]{2}\/[0-9]{4}\).   { yylval.string = yytext; return DATE; }
 done                                { return DONE; }
