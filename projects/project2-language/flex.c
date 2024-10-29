35d77764888393e71a100f5cb48c5c42ce680cb7
gbaksa22 <gbaksa22@gmail.com>
2024-10-29 11:39:48 -0400

    fixed spacing

--- projects/project2-language/flex.c
+++ projects/project2-language/flex.c
@@ -10,10 +10,10 @@ new                                 { return NEW; }
 #[A-Z][a-z]+.                       { yylval.string = yytext[1]; return CATEGORY; }
 [0-9]+.                             { yylval.number = atof(yytext); return AMOUNT; }
 [0-9]+\.[0-9]{2}.                   { yylval.number = atof(yytext); return AMOUNT; }
-\$income.                            { yylval.string = yytext; return INCOME; }
-\$\+.                                { yylval.string = yytext; return INCOME; }
-\$expense.                           { yylval.string = yytext; return EXPENSE; }
-\$\-.                                { yylval.string = yytext; return EXPENSE; }
+\$income.                           { yylval.string = yytext; return INCOME; }
+\$\+.                               { yylval.string = yytext; return INCOME; }
+\$expense.                          { yylval.string = yytext; return EXPENSE; }
+\$\-.                               { yylval.string = yytext; return EXPENSE; }
 \([0-9]{2}-[0-9]{2}-[0-9]{4}\).     { yylval.string = yytext; return DATE; }
 \([0-9]{2}\/[0-9]{2}\/[0-9]{4}\).   { yylval.string = yytext; return DATE; }
 done                                { return DONE; }
