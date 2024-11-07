116b97738342dbb758b243d1e0da86be08a77dd0
preston-speidel <125756232+preston-speidel@users.noreply.github.com>
2024-11-07 12:15:36 -0500

    locations class

--- projects/project3-zork/Location.h
+++ projects/project3-zork/Location.h
@@ -1,32 +1,45 @@
 #ifndef LOCATION_H
 #define LOCATION_H
 
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
 #include "NPC.h"
 #include "Item.h"
 
 class Location {
     public:
         // Constructor
-        Location(std::string name, std::string description);
+        Location(const std::string& name, const std::string& description);
 
         // Getters
-        std::map<string, Location> get_locations();
-        std::vector<NPC> get_npcs();
-        std::vector<Item> get_items();
+        std::map<std::string, Location*> get_locations() const;
+        std::vector<NPC> get_npcs() const;
+        std::vector<Item> get_items() const;
+        bool get_visited() const;
 
-        // Adders
-        void add_location(string direction, Location location);
-        void add_npc(NPC npc);
-        void add_item(Item item);
-
-
+        // Addders
+        void add_location(const std::string& direction, Location* location);
+        void add_npc(const NPC& npc);
+        void add_item(const Item& item);
+        
+        // Set visited
         void set_visited();
 
+        // Overloaded stream operator
+        friend std::ostream& operator<<(std::ostream& os, const Location& location);
+
     private:
-
-}
-
-
-
+        std::string name;
+        std::string description;
+        bool visited = false;
+        std::map<std::string, Location*> neighbors;
+        std::vector<NPC> npcs;
+        std::vector<Item> items;
+};
 
 #endif
+
+
+
