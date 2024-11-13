07ab8cb5c79173b1a71831f9b2431f4b42cd7e2d
preston-speidel <125756232+preston-speidel@users.noreply.github.com>
2024-11-13 18:59:01 -0500

    Update Game.h

--- projects/project3-zork/src/Game.h
+++ projects/project3-zork/src/Game.h
@@ -4,42 +4,63 @@
 #include <string>
 #include <vector>
 #include <map>
-#include "Location.hpp"
-#include "Item.hpp"
-#include "NPC.hpp"
+#include "Location.h"
+#include "Item.h"
+#include "NPC.h"
 
 class Game {
-public:
-    // Constructor
-    Game();
+    public:
+        // Constructor
+        Game();
 
-    // Core game loop
-    void play();
+        // Core game loop
+        void play();
 
-    // Command setup and world creation
-    std::map<std::string, void(*)(std::vector<string>)> setup_commands();
-    void create_world();
+        // Command setup and world creation
+        std::map<std::string, void(*)(std::vector<std::string>)> setup_commands();
+        void create_world();
 
-    // Command functions
-    void show_help();
-    void talk(std::vector<std::string> target);
-    void meet(std::vector<std::string> target);
-    void take(std::vector<std::string> target);
-    void give(std::vector<std::string> target);
-    void go(std::vector<std::string> target);
-    void show_items(std::vector<std::string> target);
-    void look(std::vector<std::string> target);
-    void quit(std::vector<std::string> target);
-    void build(std::vector<std::string> target);
+        // Command functions
+        void show_help();
+        void talk(std::vector<std::string> target);
+        void meet(std::vector<std::string> target);
+        void take(std::vector<std::string> target);
+        void give(std::vector<std::string> target);
+        void go(std::vector<std::string> target);
+        void show_items(std::vector<std::string> target);
+        void look(std::vector<std::string> target);
+        void quit(std::vector<std::string> target);
+        void build(std::vector<std::string> target);
 
-private:
-    std::map<std::string, void(*)(std::vector<string>)> commands;
-    std::vector<Item> inventory;
-    int current_weight = 0;
-    std::vector<Location> locations;
-    Location* current_location;
-    int needed_parts = 4; 
-    bool game_in_progress = true;
+    private:
+        std::map<std::string, void(*)(std::vector<std::string>)> commands;
+        std::vector<Item> inventory;
+        std::vector<Location> locations;
+        Location* current_location;
+        int needed_parts = 5; 
+        bool game_in_progress = true;
+
+        Location flynnFletcherResidence;
+        Location backyardWorkshop;
+        Location doofenshmirtzEvilInc;
+        Location constructionSite;
+        Location baljeetHouse;
+        Location bufordHouse;
+        Location danvillePark;
+        Location secretLair;
+
+        NPC phineas;
+        NPC candace;
+        NPC constructionWorker;
+        NPC drDoofenshmirtz;
+        NPC majorMonogram;
+        
+        Item blowtorch;
+        Item peanutButter;
+        Item wrench;
+        Item roboMachine;
+        Item computerChip;
+        Item blueprints;
 };
 
 #endif
