85db80afaa1617a1bcd24002e57dd692e6753fe7
gbaksa22 <gbaksa22@gmail.com>
2024-11-14 22:48:48 -0500

    removed commented audio in main

--- projects/project3-zork/src/main.cpp
+++ projects/project3-zork/src/main.cpp
@@ -11,27 +11,4 @@ int main() {
     Game game;
     game.play();
     return 0;
-
-    /*
-    std::vector<std::string> npcSounds;
-    npcSounds.push_back("./audio/doofenshmirtz/doofenshmirtz-evil-incorporated-jingle.wav");
-    npcSounds.push_back("./audio/doofenshmirtz/getting-warmer.wav");
-    npcSounds.push_back("./audio/doofenshmirtz/perry-the-platypus.wav");
-    npcSounds.push_back("./audio/doofenshmirtz/red-button.wav");
-
-    AudioManager npcAudioManager(npcSounds);
-
-    if (!npcAudioManager.init()) {
-        return -1;
-    }
-
-    npcAudioManager.playRandomSound();
-
-    std::cout << "Press Enter to quit...";
-    std::cin.get();
-
-    npcAudioManager.cleanup();
-
-    return 0;
-    */
 }
